d23f27c2486510a47490ff6c59fad02fa59ef838
Manish Devrani <manishdevrani777@gmail.com>
2022-05-02 17:41:51 +0530

    setup tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Home page</h1>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+        <div>
+          <div className="text-xl font-medium text-black">ChitChat</div>
+          <p className="text-slate-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 };
