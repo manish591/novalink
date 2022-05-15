d305f0fd4c90dde14736c7fc618eaebd1281ea5d
Manish Devrani <manishdevrani777@gmail.com>
2022-05-15 15:10:42 +0530

    added navigation component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { SearchBar } from 'components';
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0 py-2 lg:py-3 text-[#2F394D] bg-white z-[10]">
+      <section className="flex justify-between items-center w-11/12 m-auto">
+        <div className="lg:hidden">
+          <h1 className="text-2xl sm:text-3xl font-['Pacifico']">novalink</h1>
+        </div>
+        <div className="flex gap-3 lg:gap-5 items-center lg:w-full">
+          <SearchBar />
+          <section className="flex items-center justify-center ml-auto">
+            <span className="material-symbols-rounded text-2xl">
+              notifications
+            </span>
+          </section>
+          <section className="flex items-center justify-center translate-y-0.5">
+            <span className="material-symbols-rounded text-xl">chat</span>
+          </section>
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export { Navbar };
