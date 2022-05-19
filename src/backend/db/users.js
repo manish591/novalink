c9721148429097f4d076cd1d1affd99c75f5812f
Manish Devrani <manishdevrani777@gmail.com>
2022-05-20 04:38:01 +0530

    updated backend

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,5 +1,5 @@
-import { v4 as uuid } from "uuid";
-import { formatDate } from "../utils/authUtils";
+import { v4 as uuid } from 'uuid';
+import { formatDate } from '../utils/authUtils';
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
@@ -8,10 +8,15 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    username: "adarshbalika",
-    password: "adarshBalika123",
+    firstName: 'Adarsh',
+    lastName: 'Balika',
+    username: 'adarshbalika',
+    password: 'adarshBalika123',
+    bio: 'Be yourself!',
+    bookmarks: [],
+    avatarUrl:
+      'https://res.cloudinary.com/dtrjdcrme/image/upload/v1651473734/socialmedia/avatars/adarsh-balika_dct6gm.webp',
+    website: 'https://manish-dev.netlify.app/',
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
