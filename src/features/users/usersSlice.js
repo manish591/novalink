9c83d9919173b0398952c5a03787b1365b83dafb
Manish Devrani <manishdevrani777@gmail.com>
2022-05-22 14:42:40 +0530

    created get all users async thunk function

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,11 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { API_STATE } from 'common';
+import { getAllUsers } from 'services';
 
 const initialState = {
   usersStatus: API_STATE.IDLE,
   usersData: [],
   usersError: '',
 };
+
+const getUsers = createAsyncThunk(
+  'users/get',
+  async (_, { rejectWithValue }) => {
+    try {
+      const res = getAllUsers();
+      return (await res).data;
+    } catch (err) {
+      console.error(err);
+      return rejectWithValue(err.responce.data);
+    }
+  },
+);
 
 const usersSlice = createSlice({
   name: 'users',
@@ -14,4 +28,5 @@ const usersSlice = createSlice({
   extraReducers: {},
 });
 
+export { getUsers };
 export const usersReducer = usersSlice.reducer;
