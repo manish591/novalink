391db7c12ab851f5e9ff1f2e10c23cef55113838
Manish Devrani <manishdevrani777@gmail.com>
2022-05-22 11:06:22 +0530

    updated the users database

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -7,16 +7,32 @@ import { formatDate } from '../utils/authUtils';
 
 export const users = [
   {
-    _id: uuid(),
+    _id: '83d92b58-5c52-453f-b865-04638127e76f',
     firstName: 'Adarsh',
     lastName: 'Balika',
     username: 'adarshbalika',
     password: 'adarshBalika123',
     bio: 'Be yourself!',
     bookmarks: [],
-    avatarUrl:
-      'https://res.cloudinary.com/dtrjdcrme/image/upload/v1651473734/socialmedia/avatars/adarsh-balika_dct6gm.webp',
+    avatarUrl: 'https://i.pravatar.cc/150?img=23',
     website: 'https://manish-dev.netlify.app/',
+    coverImg:
+      'https://res.cloudinary.com/dcugqfvvg/image/upload/v1653006259/priscilla-du-preez-GobsYxc_H_0-unsplash_1_gcgcve.jpg',
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+  {
+    _id: '83d92b58-5c52-453f-b865-04838127e76f',
+    firstName: 'Manish',
+    lastName: 'Devrani',
+    username: 'manish18',
+    password: 'manishdevrani123456',
+    bio: 'Never regret anything that made you smile.',
+    bookmarks: [],
+    avatarUrl: 'https://i.pravatar.cc/150?img=35',
+    website: 'https://manish-dev.netlify.app/',
+    coverImg:
+      'https://res.cloudinary.com/dcugqfvvg/image/upload/v1653006259/priscilla-du-preez-GobsYxc_H_0-unsplash_1_gcgcve.jpg',
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
