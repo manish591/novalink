781172428acd5ac5dbad063418a9b57c6d0a8795
Manish Devrani <manishdevrani777@gmail.com>
2022-05-22 11:51:14 +0530

    create get posts async thunk function

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { API_STATE } from 'common';
+import { getAllPosts } from 'services';
 
 const initialState = {
   postStatus: API_STATE.IDLE,
   postError: '',
   postData: [],
 };
+
+const getPosts = createAsyncThunk('posts/get', async ({ rejectWithValue }) => {
+  try {
+    const res = await getAllPosts();
+    return res.data;
+  } catch (err) {
+    console.error(err);
+    return rejectWithValue(err.response.data);
+  }
+});
 
 const postSlice = createSlice({
   name: 'posts',
@@ -14,4 +25,5 @@ const postSlice = createSlice({
   extraReducers: {},
 });
 
+export { getPosts };
 export const postReducer = postSlice.reducer;
