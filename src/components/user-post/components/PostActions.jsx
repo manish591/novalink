7c0ea84f2cd446a7c274dfd8073be933ff5ac293
Manish Devrani <manishdevrani777@gmail.com>
2022-05-23 19:39:44 +0530

    style-updated the border radius of top and bottom element

--- src/components/user-post/components/PostActions.jsx
+++ src/components/user-post/components/PostActions.jsx
@@ -4,22 +4,30 @@ import PropTypes from 'prop-types';
 const PostActions = ({ setOpenPostActions }) => {
   return (
     <div className="fixed inset-0 bg-[rgba(25,25,25,50%)] z-[30]">
-      <div className="grid grid-cols-1 absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 w-max bg-[white] rounded-md">
-        <button type="button" className="py-2.5 px-20 border-b text-red-600">
+      <div className="grid grid-cols-1 absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 w-max bg-[white] rounded-xl">
+        <button
+          type="button"
+          className="py-2.5 px-20 lg:px-32 lg:py-3.5 border-b text-red-600 hover:bg-gray-100 first:rounded-t-xl">
           Delete
         </button>
-        <button type="button" className="py-2.5 px-20 border-b">
+        <button
+          type="button"
+          className="py-2.5 px-20 lg:px-32 lg:py-3.5 border-b hover:bg-gray-100">
           Edit
         </button>
-        <button type="button" className="py-2.5 px-20 border-b">
+        <button
+          type="button"
+          className="py-2.5 px-20 lg:px-32 lg:py-3.5 border-b hover:bg-gray-100">
           Go To Post
         </button>
-        <button type="button" className="py-2.5 px-20 border-b">
+        <button
+          type="button"
+          className="py-2.5 px-20 lg:px-32 lg:py-3.5 border-b hover:bg-gray-100">
           Copy Link
         </button>
         <button
           type="button"
-          className="py-2.5 px-20 border-b last:border-none"
+          className="py-2.5 px-20 lg:px-32 lg:py-3.5 border-b last:border-none last:rounded-b-xl hover:bg-gray-100"
           onClick={() => {
             setOpenPostActions((opa) => !opa);
           }}>
